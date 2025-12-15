3927afc2c37216e4917d2eb3a83ed4aa9ea13a46
bala-amypo <bala@amypo.in>
2025-12-15 10:41:33 +0000

    User commit

--- src/main/java/com/example/demo/model/Host.java
+++ src/main/java/com/example/demo/model/Host.java
@@ -1,7 +1,82 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
 public class Host {
     private Long id;
     private String hostName;
-    private String 
+    private String fullname;
+    private String email;
+    private String department;
+    private String phone;
+    private LocalDateTime createdAt;
+
+    public Host(){}
+
+    public Host(String hostName, String fullname, String email, String department, String phone,
+            LocalDateTime createdAt) {
+        this.hostName = hostName;
+        this.fullname = fullname;
+        this.email = email;
+        this.department = department;
+        this.phone = phone;
+        this.createdAt = createdAt;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    
 }
