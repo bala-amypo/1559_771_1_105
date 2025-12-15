011712aece586a39fe77e0d8b514eba86d25b7a8
bala-amypo <bala@amypo.in>
2025-12-15 10:32:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+public class Visitor {
+    private long id;
+    private String fullname;
+    private String email;
+    private String phone;
+    private String idProofN
+}
