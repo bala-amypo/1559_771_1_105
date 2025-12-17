6baf5098087bc66faeacd63034e806ead9db5951
bala-amypo <bala@amypo.in>
2025-12-17 05:24:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,37 @@
+package com.example.visitor.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class User {
+   
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private LocalDateTime createdAt;
+
+    public User() {}
+
+    public User(String username, String email, String password, String role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+  
+
+    public Long getId() { return id; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+}
