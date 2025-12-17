14d134cb9a2eca6584a0b5033bcd7b3b1a67a07b
bala-amypo <bala@amypo.in>
2025-12-17 05:19:18 +0000

    User commit

--- src/main/java/com/example/demo/model/AlertNotification.java
+++ src/main/java/com/example/demo/model/AlertNotification.java
@@ -5,13 +5,8 @@ import java.time.LocalDateTime;
 
 
 public class AlertNotification {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @OneToOne
     private VisitLog visitLog;
-
     private String sentTo;
     private String alertMessage;
     private LocalDateTime sentAt;
@@ -24,18 +19,33 @@ public class AlertNotification {
         this.alertMessage = alertMessage;
     }
 
-    @PrePersist
-    private void onSend() {
-        this.sentAt = LocalDateTime.now();
-    }
+   
 
-    // Getters and Setters
-    public Long getId() { return id; }
-    public VisitLog getVisitLog() { return visitLog; }
-    public void setVisitLog(VisitLog visitLog) { this.visitLog = visitLog; }
-    public String getSentTo() { return sentTo; }
-    public void setSentTo(String sentTo) { this.sentTo = sentTo; }
-    public String getAlertMessage() { return alertMessage; }
-    public void setAlertMessage(String alertMessage) { this.alertMessage = alertMessage; }
-    public LocalDateTime getSentAt() { return sentAt; }
+    public Long getId() { 
+        return id; 
+    }
+    public void setId() {
+        this.id = id;
+    }
+    public VisitLog getVisitLog() { 
+        return visitLog; 
+    }
+    public void setVisitLog(VisitLog visitLog) { 
+        this.visitLog = visitLog; 
+    }
+    public String getSentTo() { 
+        return sentTo; 
+    }
+    public void setSentTo(String sentTo) { 
+        this.sentTo = sentTo; 
+    }
+    public String getAlertMessage() { 
+        return alertMessage; 
+    }
+    public void setAlertMessage(String alertMessage) { 
+        this.alertMessage = alertMessage; 
+    }
+    public LocalDateTime getSentAt() { 
+        return sentAt; 
+    }
 }
