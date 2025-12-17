78c9ce64e6b63d3c7b6e94508a0e932d021eab48
bala-amypo <bala@amypo.in>
2025-12-17 04:59:18 +0000

    User commit

--- src/main/java/com/example/demo/model/Appointment.java
+++ src/main/java/com/example/demo/model/Appointment.java
@@ -1,14 +1,42 @@
-package com.example.demo.model
+package com.example.visitor.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
 
-public class Appointment{
+@Entity
+public class Appointment {
+   
     private Long id;
     private Visitor visitor;
     private Host host;
     private LocalDate appointmentDate;
     private String purpose;
-    private String status;
+    private String status = "SCHEDULED";
+
+    public Appointment() {}
+
+    public Appointment(Visitor visitor, Host host, LocalDate appointmentDate, String purpose) {
+        this.visitor = visitor;
+        this.host = host;
+        this.appointmentDate = appointmentDate;
+        this.purpose = purpose;
+        this.status = "SCHEDULED";
+    }
+
+    
+  
+       
 
 
-    public Appointment () {}
+    public Long getId() { return id; }
+    public Visitor getVisitor() { return visitor; }
+    public void setVisitor(Visitor visitor) { this.visitor = visitor; }
+    public Host getHost() { return host; }
+    public void setHost(Host host) { this.host = host; }
+    public LocalDate getAppointmentDate() { return appointmentDate; }
+    public void setAppointmentDate(LocalDate appointmentDate) { this.appointmentDate = appointmentDate; }
+    public String getPurpose() { return purpose; }
+    public void setPurpose(String purpose) { this.purpose = purpose; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
