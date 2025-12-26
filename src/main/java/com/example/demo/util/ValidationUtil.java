2045a293ec680f26b20b1f4c2e220f7a9e7f5eae
bala-amypo <bala@amypo.in>
2025-12-26 13:46:21 +0000

    User commit

--- src/main/java/com/example/demo/util/ValidationUtil.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.util;
-
-public class ValidationUtil {
-
-    public static boolean isNullOrEmpty(String val) {
-        return val == null || val.trim().isEmpty();
-    }
-}
