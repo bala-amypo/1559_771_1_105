bb5a79ffd076533a2b5451988a0d362e43786394
bala-amypo <bala@amypo.in>
2025-12-27 05:00:56 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -2,11 +2,18 @@ package com.example.demo.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
-    private String secret; [cite: 362]
-    private Long jwtExpirationMs; [cite: 363]
+
+    @Value("${com.example.demo.security.secret}")
+    private String secret;
+
+    @Value("${com.example.demo.security.jwtExpirationMs}")
+    private Long jwtExpirationMs;
 
     public String generateToken(String username, String role, Long userId, String email) {
         return Jwts.builder()
@@ -17,13 +24,13 @@ public class JwtUtil {
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
-                .compact(); [cite: 365, 369, 370, 371]
+                .compact();
     }
 
     public Jws<Claims> validateAndGetClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(secret.getBytes())
                 .build()
-                .parseClaimsJws(token); [cite: 366]
+                .parseClaimsJws(token);
     }
 }
