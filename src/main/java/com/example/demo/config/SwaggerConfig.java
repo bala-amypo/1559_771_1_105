bf9fd84c9c85ac46fb9c9c9a2c53df8f78a73f1b
bala-amypo <bala@amypo.in>
2025-12-27 03:42:16 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,25 +1,25 @@
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.info.Info;
+// import io.swagger.v3.oas.models.servers.Server;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+// import java.util.List;
 
-@Configuration
-public class SwaggerConfig {
+// @Configuration
+// public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Visitor Management API")
-                        .version("1.0")
-                        .description("APIs for visitor management, authentication, and alerts"))
-                .servers(List.of(
-                        new Server().url("https://9083.pro604cr.amypo.ai")
-                ));
-    }
-}
+//     @Bean
+//     public OpenAPI customOpenAPI() {
+//         return new OpenAPI()
+//                 .info(new Info()
+//                         .title("Visitor Management API")
+//                         .version("1.0")
+//                         .description("APIs for visitor management, authentication, and alerts"))
+//                 .servers(List.of(
+//                         new Server().url("https://9083.pro604cr.amypo.ai")
+//                 ));
+//     }
+// }
